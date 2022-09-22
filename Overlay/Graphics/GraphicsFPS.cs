9aff481a90f131ef3a47c7f926c07bbcb124b408
Cosmic-1 <crossdoh@gmail.com>
2022-09-22 22:07:54 +0300

    Correct FPS

--- Overlay/Graphics/GraphicsFPS.cs
+++ Overlay/Graphics/GraphicsFPS.cs
@@ -5,34 +5,53 @@
         readonly Font font;
         readonly Brush brush;
         readonly PointF pointF;
-        readonly Stopwatch sw;
-        int fpsCount;
-        string renderStrFps = "FPS:";
+        readonly FPS fps;
         public GraphicsFPS()
         {
-            font = new("Arial", 30);
+            font = new("Arial", 16);
             brush = Brushes.Gold;
             pointF = new PointF(10, 10);
-            sw = Stopwatch.StartNew();
+            fps = new();
         }
-        private void Update()
-        {
-            if (sw.ElapsedMilliseconds <= 1000)
-            {
-                fpsCount++;
-            }
-            else
-            {
-                renderStrFps = $"FPS: {fpsCount}";
-                fpsCount = 0;
-                sw.Restart();
-            }
-        }
+
 
         public void Render(PaintEventArgs e)
         {
-            Update();
-            e.Graphics.DrawString(renderStrFps, font, brush, pointF);
+            fps.Update();
+            e.Graphics.DrawString(fps.ToString(), font, brush, pointF);
+        }
+    }
+
+    public class FPS
+    {
+        static readonly TimeSpan FpsUpdate = TimeSpan.FromSeconds(1);
+
+        Stopwatch sw;
+        int fpsCount;
+        double fps;
+        public double Fps => fps;
+
+        public FPS()
+        {
+            sw = Stopwatch.StartNew();
+        }
+
+        public void Update()
+        {
+            var fpsTimerMs = sw.Elapsed;
+            if (fpsTimerMs > FpsUpdate)
+            {
+                fps = fpsCount / fpsTimerMs.TotalSeconds;
+                sw.Restart();
+                fpsCount = 0;
+            }
+
+            fpsCount++;
+        }
+
+        public override string ToString()
+        {
+            return $"FPS: {fps}";
         }
     }
 }
